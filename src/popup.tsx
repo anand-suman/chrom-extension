121a5ee9f60a56d2cb49485a2b94c0eba08a7360
Anand Kumar Suman <anandraj0705@gmail.com>
2024-10-04 22:57:09 +0530

    first commit

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,20 +1,11 @@
-import { useState } from "react"
+import { CountButton } from "~features/CountButton"
+
+import "~style.css"
 
 function IndexPopup() {
-  const [data, setData] = useState("")
-
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        padding: 16
-      }}>
-      <h1>
-        Welcome to your <a href="https://www.plasmo.com">Plasmo</a> Extension!
-      </h1>
-      <input onChange={(e) => setData(e.target.value)} value={data} />
-      <footer>Crafted by @PlasmoHQ</footer>
+    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
+      <CountButton />
     </div>
   )
 }
